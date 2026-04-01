6a41de57e75b47d34c4f7ac2622d5aae4acf1c2b
Pooja-Cloudy <poojashelke1011@outlook.com>
2026-04-01 09:24:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region  = "ap-southeast-2"
-  profile = "mahesh"
+  profile = "Pooja"
 
 }
 
 terraform {
   backend "s3" {
-    bucket  = "aws-mahesh-bucket-922231562522-ap-southeast-2-an"
+    bucket  = "aws-Pooja-bucket-922231562522-ap-southeast-2-an"
     region  = "ap-southeast-2"
     key     = "backend/terraform.tfstate"
     profile = "Pooja"
